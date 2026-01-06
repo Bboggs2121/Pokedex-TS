95b07460ac81442971c0ac16e6601f714c7f59bf
Bboggs2121 <bboggs2121@gmail.com>
2026-01-06 10:44:52 -0500

    Initialized project

---
+++ dist/commands.js
@@ -0,0 +1,39 @@
+import { commandHelp } from './command_help.js';
+import { commandExit } from './command_exit.js';
+import { commandMapBack, commandMapForward } from './command_map.js';
+import { commandExplore } from './command_explore.js';
+import { commandCatch } from './command_catch.js';
+export function getCommands() {
+    return {
+        help: {
+            name: 'help',
+            description: 'Displays a help message',
+            callback: commandHelp,
+        },
+        exit: {
+            name: 'exit',
+            description: 'Exit the Pokedex',
+            callback: commandExit,
+        },
+        map: {
+            name: 'map',
+            description: 'Get the next page of locations',
+            callback: commandMapForward,
+        },
+        mapb: {
+            name: 'mapb',
+            description: 'Get the previous page of locations',
+            callback: commandMapBack,
+        },
+        explore: {
+            name: 'explore <location_name>',
+            description: 'Explore a location',
+            callback: commandExplore,
+        },
+        catch: {
+            name: 'catch <pokemon_name>',
+            description: 'Attempt to catch a pokemon',
+            callback: commandCatch,
+        },
+    };
+}
